518e39b8303946aa31eac6d83469d45775b735e0
MMelchiorri <mm.melchiorri@gmail.com>
2025-05-29 15:17:28 +0200

    feat: implement TodoPage component to display todos in a table format

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,6 +1,44 @@
+import { fetchTodos } from "@/services/todoFetch";
+import { Todo } from "@/type/Todo";
+import {
+    Table,
+    TableContainer,
+    Paper,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+} from "@mui/material";
 
-export default async function TodoPage()  {
-    return <h1>
-        Todo Page
-    </h1>
+export default async function TodoPage() {
+
+    const todos: Todo[] = await fetchTodos();
+    const keys = Object.keys(todos[0]) as (keyof Todo)[];
+
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        {keys.map((key) => (
+                            <TableCell key={key}>{key.toUpperCase()}</TableCell>
+                        ))}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {todos.map((todo) => (
+                        <TableRow key={todo.id}>
+                            {keys.map((key) => (
+                                <TableCell key={key}>
+                                    {todo[key] instanceof Date
+                                        ? todo[key].toISOString().split("T")[0]
+                                        : todo[key]?.toString() ?? "N/A"}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
