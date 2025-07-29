7cb311025c1d623dd02f47a4652ec3cebe8db9f3
MMelchiorri <mm.melchiorri@gmail.com>
2025-07-29 17:20:10 +0200

    feat: add Todo details section with translations and layout in detail page

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -1,8 +1,30 @@
+import Grid from "@mui/material/Grid";
+import { CardHeader, Card, CardContent } from "@mui/material";
+import { getTranslations } from "next-intl/server";
+import { Paper } from "@mui/material";
+
 export default async function Page() {
+  const t = await getTranslations("Todos");
   return (
-    <div>
-      <h1>Todo Page</h1>
-      <p>This is the todo detail page.</p>
-    </div>
+    <Grid
+      container
+      spacing={2}
+      display={"flex"}
+      justifyContent={"center"}
+      sx={{ mt: 4 }}
+    >
+      <Card>
+        <Grid size={{ xs: 12 }}>
+          <CardHeader
+            title={t("Details.title")}
+            sx={{
+              p: 2,
+              textAlign: "center",
+            }}
+          />
+        </Grid>
+        <CardContent></CardContent>
+      </Card>
+    </Grid>
   );
 }
