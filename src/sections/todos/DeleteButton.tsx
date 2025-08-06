2a8fca9c225e9135be4c24de7179bc6b859be8be
MMelchiorri <mm.melchiorri@gmail.com>
2025-08-06 15:54:48 +0200

    refactor: update delete button to remove users instead of todos

--- src/sections/todos/DeleteButton.tsx
+++ src/sections/todos/DeleteButton.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import DeleteIcon from "@mui/icons-material/Delete";
-import { deleteTodo } from "@/services/todosFetch";
+import { deleteUser } from "@/services/usersFetch";
 
 export default function DeleteButton({ id }: { id: string }) {
   const handleDelete = async () => {
     try {
-      await deleteTodo(id);
+      await deleteUser(id);
       window.location.reload();
     } catch (error) {
       console.error("Failed to delete:", error);
