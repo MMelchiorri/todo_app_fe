e5bc58aa1d9d490b6391e3696b7a05ba202f5640
MMelchiorri <mm.melchiorri@gmail.com>
2025-08-06 15:05:44 +0200

    refactor: enhance user details page with job status and general info display

--- src/app/users/[_id]/page.tsx
+++ src/app/users/[_id]/page.tsx
@@ -1,5 +1,5 @@
 import { getUserById } from "@/services/usersFetch";
-import { Box, IconButton, Typography } from "@mui/material";
+import { Box, Chip, IconButton, Typography } from "@mui/material";
 import { getTranslations } from "next-intl/server";
 import EditIcon from "@mui/icons-material/Edit";
 import BackButton from "@/sections/BackButton";
@@ -9,14 +9,14 @@ export default async function Page({ params }: { params: { _id: string } }) {
   const { _id } = params;
   const user = await getUserById(_id);
   const jobAssigned = await Promise.all(
-    user?.jobAssigned?.map(async (job) => {
-      return await getTodoById(job);
-    }) || [],
+    user?.jobAssigned?.map(async (jobId) => await getTodoById(jobId)) || [],
   );
   const t = await getTranslations("Users");
+
   if (!user) {
     return <Typography>{t("details.notFound")}</Typography>;
   }
+
   return (
     <Box
       sx={{
@@ -24,7 +24,7 @@ export default async function Page({ params }: { params: { _id: string } }) {
         backgroundColor: "white",
         borderRadius: 4,
         p: 3,
-        width: "60%",
+        width: "50%",
         mx: "auto",
         mt: 4,
       }}
@@ -33,31 +33,50 @@ export default async function Page({ params }: { params: { _id: string } }) {
         display="flex"
         alignItems="center"
         mb={2}
-        justifyContent={"space-between"}
+        justifyContent="space-between"
       >
         <BackButton />
         <IconButton size="small" sx={{ color: "black" }}>
           <EditIcon fontSize="small" />
         </IconButton>
       </Box>
-      <Box
-        display={"flex"}
-        alignItems={"center"}
-        justifyContent={"space-between"}
-      >
+
+      <Box display="flex" justifyContent="space-between">
         <Typography variant="h6" fontWeight="bold">
-          {user?.username}
+          {user.username}
         </Typography>
         <Typography variant="h6" fontWeight="bold">
-          {user?.role}
+          {user.role}
         </Typography>
       </Box>
-      {user.jobAssigned && (
-        <Box>
+
+      {jobAssigned.length > 0 && (
+        <Box mt={5}>
           {jobAssigned.map((job) => (
-            <Typography variant="body2" key={job?._id}>
-              {job?.name} - {job?.description}
-            </Typography>
+            <Box key={job?._id} mt={4}>
+              <Typography variant="h5">{t("details.generalInfo")}</Typography>
+              <Typography variant="h6">
+                {job?.name} – {job?.description}
+              </Typography>
+
+              <Typography variant="h5" mt={3}>
+                {t("details.status")}
+              </Typography>
+
+              <Box
+                display="flex"
+                gap={1}
+                mt={1}
+                justifyContent={"space-between"}
+              >
+                <Box>
+                  {job?.tags?.map((tag) => <Chip label={tag} key={tag} />)}
+                </Box>
+                <Typography variant="h6">
+                  {t("details.completed")}: {job?.completed ? "✅" : "❌"}
+                </Typography>
+              </Box>
+            </Box>
           ))}
         </Box>
       )}
