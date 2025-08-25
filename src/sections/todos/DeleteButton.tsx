f34118d8db7a81e13c57d5272645fbe6aaa44f1a
MMelchiorri <mm.melchiorri@gmail.com>
2025-08-25 17:24:14 +0200

    refactor: update delete functionality to use deleteTodo service instead of deleteUser

--- src/sections/todos/DeleteButton.tsx
+++ src/sections/todos/DeleteButton.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import DeleteIcon from '@mui/icons-material/Delete'
-import { deleteUser } from '@/services/usersFetch'
+import { deleteTodo } from '@/services/todosFetch'
 
 export default function DeleteButton({ id }: { id: string }) {
   const handleDelete = async () => {
     try {
-      await deleteUser(id)
+      await deleteTodo(id)
       window.location.reload()
     } catch (error) {
       console.error('Failed to delete:', error)
