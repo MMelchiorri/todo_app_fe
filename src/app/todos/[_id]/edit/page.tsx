d59315a7d5e4573b922124ddc366b3ecade39d6e
MMelchiorri <mm.melchiorri@gmail.com>
2025-09-15 10:14:23 +0200

    feat: rename CreateTodoForm to TodoForm for consistency in component naming

--- src/app/todos/[_id]/edit/page.tsx
+++ src/app/todos/[_id]/edit/page.tsx
@@ -1,9 +1,9 @@
 import { getTodoById } from '@/services/todosFetch'
-import CreateTodoForm from '@/sections/todos/FormDataTodos'
+import TodoForm from '@/sections/todos/FormDataTodos'
 
 export default async function Page({ params }: { params: { _id: string } }) {
   const { _id } = params
   const todo = await getTodoById(_id)
 
-  return <CreateTodoForm todo={todo} />
+  return <TodoForm todo={todo} />
 }
