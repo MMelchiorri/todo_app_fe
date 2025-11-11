ab6cdaecf25796587e4205eaa2f6dad8fa31a91f
MMelchiorri <mm.melchiorri@gmail.com>
2025-11-11 10:51:32 +0100

    feat: update Navbar component with internationalization and improved layout

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -2,18 +2,36 @@
 
 import Link from 'next/link'
 import styles from '@/styles/navbar.module.css'
+import { HomeFilled, FormatListBulleted, Person } from '@mui/icons-material'
+import { Box } from '@mui/material'
+import { Inter } from 'next/font/google'
+import { useTranslations } from 'next-intl'
+
+const inter = Inter({ subsets: ['latin'], weight: ['200'] })
 
 export function Navbar() {
+  const t = useTranslations('Todos')
   return (
-    <nav className={`${styles.navbar} `}>
+    <nav className={`${styles.navbar} ${inter.className} `}>
       <div className={`${styles.container}`}>
-        <h2 className={`${styles.title}`}>Todo App</h2>
         <div className={`${styles.innerContainer}`}>
+          <Link className={styles.link} href={'/'}>
+            <Box display={'flex'} alignItems={'center'} gap={2}>
+              <HomeFilled fontSize={'small'} />
+              <p className={`${styles.title}`}>{t('navbar.home')}</p>
+            </Box>
+          </Link>
           <Link className={`${styles.link}`} href="/todos">
-            Todos
+            <Box display={'flex'} alignItems={'center'} gap={2}>
+              <FormatListBulleted fontSize={'small'} />
+              <p className={`${styles.title}`}>{t(`navbar.todo`)}</p>
+            </Box>
           </Link>
           <Link className={`${styles.link}`} href="/users">
-            User
+            <Box display={'flex'} alignItems={'center'} gap={2}>
+              <Person fontSize={'small'} />
+              <p className={`${styles.title}`}>{t(`navbar.userProfile`)}</p>
+            </Box>
           </Link>
         </div>
       </div>
